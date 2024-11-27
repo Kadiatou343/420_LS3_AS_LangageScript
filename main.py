28e797c73a0eb95949c9b1fdd9ef2bec81814316
maguette03 <maguette_laye.thiaw_tall@lcieducation.net>
2024-11-27 15:20:44 -0500

    finition des crud pour les produits

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from Business.Domain.User import User
 from Data_Access.DAOs.BeverageDAO import BeverageDAO
 from Data_Access.DAOs.GroceryDAO import GroceryDAO
 from Data_Access.DAOs.TreatDAO import TreatDAO
+from Data_Access.database import cursor
 
 treat_dao = TreatDAO()
 groc_dao = GroceryDAO()
@@ -104,6 +105,71 @@ def edit_grocery(id):
     grocery = groc_dao.get_by_id(id)
     return render_template('addGrocery.html', grocery=grocery)
 
+@app.route('/updateGrocery', methods=['POST', 'GET'])
+@app.route('/updateTreat', methods=['POST', 'GET'])
+@app.route('/updateBeverage', methods=['POST', 'GET'])
+def update_pro():
+    route = request.path
+
+    if route == '/updateGrocery':
+        if request.method == 'POST':
+            prod_name = request.form['prodName']
+            unit_price = request.form['unitPrice']
+            qty_per_pack = request.form['qtyPerPack']
+            exp_year = request.form['expYear']
+            origine = request.form['origine']
+            id = request.form['id']
+
+            grocery = Grocery(id, prod_name, unit_price, qty_per_pack, exp_year, origine)
+            groc_dao.update_grocery(grocery)
+
+            return redirect(url_for('product'))
+
+    elif route == '/updateTreat':
+        if request.method == 'POST':
+            prod_name = request.form['prodName']
+            unit_price = request.form['unitPrice']
+            qty_per_pack = request.form['qtyPerPack']
+            exp_year = request.form['expYear']
+            treat_type = request.form['treatType']
+            id = request.form['id']
+
+            treat = Treat(id, prod_name, unit_price, qty_per_pack, exp_year, treat_type)
+            treat_dao.update_treat(treat)
+
+            return redirect(url_for('product'))
+    elif route == '/updateBeverage':
+        if request.method == 'POST':
+            prod_name = request.form['prodName']
+            unit_price = request.form['unitPrice']
+            qty_per_pack = request.form['qtyPerPack']
+            exp_year = request.form['expYear']
+            beverage_type = request.form['beverageType']
+            id = request.form['id']
+
+            bev = Beverage(id, prod_name, unit_price, qty_per_pack, exp_year, beverage_type)
+            bev_dao.update_beverage(bev)
+
+            return redirect(url_for('product'))
+
+    return redirect(url_for('product'))
+
+@app.route('/deleteTreat/<int:id>')
+def del_treat(id):
+    treat = treat_dao.get_by_id(id)
+    treat_dao.delete_treat(treat)
+    return redirect(url_for('product'))
+@app.route('/deleteGrocery/<int:id>')
+def del_grocery(id):
+    grocery = groc_dao.get_by_id(id)
+    groc_dao.delete_grocery(grocery)
+    return redirect(url_for('product'))
+
+@app.route('/deleteBeverage/<int:id>')
+def del_beverage(id):
+    beverage = bev_dao.get_by_id(id)
+    bev_dao.delete_beverage(beverage)
+    return redirect(url_for('product'))
 
 if __name__ == '__main__':
     app.run(debug=True)
